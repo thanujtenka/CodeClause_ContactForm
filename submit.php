66e627ffab62efae2264643a404c16451f578cf7
thanujtenka <132458230+thanujtenka@users.noreply.github.com>
2023-06-30 11:56:17 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+// Check if the captcha value is correct
+// if ($_POST['captcha'] === $_SESSION['captcha']) {
+//     echo '<script>alert("Submitted Sucessfully")</script>';
+
+// } else {
+//     echo '<script>alert("Wrong captcha")</script>';
+// }
+echo 'Hello, World!';
+?>
